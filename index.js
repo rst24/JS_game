315daac3b46668c2dbc0e34bb636c14394895a3b
Rino <stoichevrado@gmail.com>
2022-04-03 14:41:23 +0300

    more dynamics

--- index.js
+++ index.js
@@ -152,6 +152,23 @@ const enemy = new Sprite({
       height: 50
     }
 })
+console.log(player)
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    ArrowRight:{
+        pressed: false
+    },
+    ArrowLeft: {
+        pressed: false
+    }
+}
+
+decreaseTimer()
 
 function animate(){
     window.requestAnimationFrame(animate)
@@ -179,13 +196,6 @@ function animate(){
         player.switchSprite('idle')
       }
     
-      // jumping
-      if (player.velocity.y < 0) {
-        player.switchSprite('jump')
-      } else if (player.velocity.y > 0) {
-        player.switchSprite('fall')
-      }
-    
       // Enemy movement
       if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
         enemy.velocity.x = -5
@@ -196,4 +206,123 @@ function animate(){
       } else {
         enemy.switchSprite('idle')
       }
+
+      // jumping
+      if (player.velocity.y < 0) {
+        player.switchSprite('jump')
+      } else if (player.velocity.y > 0) {
+        player.switchSprite('fall')
+      }
+    //detect for colision and enemy gets hit
+      if(
+        rectangularCollision({
+            rectangle1: player,
+            rectangle2: enemy
+        }) &&
+        player.isAttacking &&
+        player.framesCurrent === 4
+    )  {
+        enemy.takeHit()
+        player.isAttacking = false
+
+        gsap.to('#enemyHealth', {
+            width: enemy.health + '%'
+          })
+    }
+
+    //if player misses
+    if(player.isAttacking && player.framesCurrent === 4){
+        player.isAttacking = false
+    }
+    
+    //if player gets hit
+
+    if(
+        rectangularCollision({
+            rectangle1: enemy,
+            rectangle2: player
+        }) &&
+        enemy.isAttacking &&
+        enemy.framesCurrent === 2
+    ){
+        player.takeHit()
+        enemy.isAttacking = false
+
+        gsap.to('#playerHealth', {
+            width: player.health + '%'
+          })
+    }
+
+    // if player misses
+     if (enemy.isAttacking && enemy.framesCurrent === 2) {
+        enemy.isAttacking = false
+    }
+
+    // end game based on health
+    if (enemy.health <= 0 || player.health <= 0) {
+        determineWinner({ player, enemy, timerId })
+     }
 }
+
+animate()
+
+window.addEventListener('keydown', (event) => {
+    if(!player.dead){
+        switch(event.key){
+            case 'd':
+                keys.d.pressed = true
+                player.lastKey = 'd'
+                break
+            case 'a':
+                keys.a.pressed = true
+                player.lastKey = 'a'
+                break
+            case 'w':
+                player.velocity.y = -20
+                break
+            case ' ':
+                player.attack()
+                break   
+        }
+    }
+    if(!enemy.dead){
+        switch(event.key){
+           case 'ArrowRight':
+        keys.ArrowRight.pressed = true
+        enemy.lastKey = 'ArrowRight'
+        break
+      case 'ArrowLeft':
+        keys.ArrowLeft.pressed = true
+        enemy.lastKey = 'ArrowLeft'
+        break
+      case 'ArrowUp':
+        enemy.velocity.y = -20
+        break
+      case 'ArrowDown':
+        enemy.attack()
+
+        break  
+        }
+    }
+})
+
+window.addEventListener('keyup', (event)=>{
+    switch (event.key) {
+        case 'd':
+          keys.d.pressed = false
+          break
+        case 'a':
+          keys.a.pressed = false
+          break
+    }
+
+    // enemy keys   
+    switch (event.key) {
+        case 'ArrowRight':
+          keys.ArrowRight.pressed = false
+          break
+        case 'ArrowLeft':
+          keys.ArrowLeft.pressed = false
+          break
+      }  
+})
